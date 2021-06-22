8c1daa54f49c74df1d29b7ca36ce749ed10f6283
Samar32 <48966753+Samar32@users.noreply.github.com>
2021-06-22 13:12:32 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,44 @@
 package exam;
 
-public class CountCombinations {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class CountCombinations
+{
+	public void getNumberOfWays(int total, int k)
+	{
+		List<Integer> combList = new ArrayList<>();
+		combinationUtil(total, k, 0, 1, combList);
+	}
 
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
+	public void combinationUtil(int n, int sum, int cs, int sn, List<Integer> combList)
+	{
+		if(cs==sum)
+		{
+			System.out.println(combList);
+			return;
+		}
 
+		for(int i=sn;i<=n;i++)
+		{
+			if(cs+i>sum)
+			{
+				break;
+			}
+			combList.add(i);
+			combinationUtil(n, sum, cs+i, i, combList);
+			combList.remove(combList.size()-1);
+		}
+	}
+
+	public static void main(String[] as)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter W: ");
+		int w = s.nextInt();
+		System.out.println("Enter T: ");
+		int t = s.nextInt();
+		CountCombinations c = new CountCombinations();
+		c.getNumberOfWays(w, t);
+	}
 }
