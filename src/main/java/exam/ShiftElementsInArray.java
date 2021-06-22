afb07ee7771a14e25f0594de92d68dddfbc3c2f2
Samar32 <48966753+Samar32@users.noreply.github.com>
2021-06-22 12:51:23 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,10 +1,36 @@
 package exam;
 
-public class ShiftElementsInArray {
+import java.util.*;
+class ShiftElementsInArray
+{
+	public static void shift(int[] arr, int n)
+	{
+		int temp;
+		int i;
+		temp = arr[n-1];
+		for(i=n-1;i>0;i--)
+		{
+			arr[i]=arr[i-1];
+		}
+		arr[0]=temp;
+		for(i=0;i<n;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
 
-  public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+	public static void main(String[] as)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the Size of Array: ");
+		int n = s.nextInt();
+		System.out.print("Enter the elements of Array: ");
+		int[] a = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i] = s.nextInt();
+		}
 
-  }
-
+		shift(a, n);
+	}
 }
